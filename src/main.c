e26454f5d5784322c0c123be0c17cf5665eab896
Steve Jeong <how2soft@gmail.com>
2023-01-20 15:21:06 +0900

    improve main source.
    Set basic frame of the package program.
    Add command option.
    
    Signed-off-by: Steve Jeong <how2soft@gmail.com>
    Change-Id: Ia65cd90876adefc24136dd486a7242fa9b55c823

--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
+#include <getopt.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int main()
+static int options;
+const char* sopts = "h";
+static struct option lopts[] = {
+    {"help", 0, 0, 'h'},
+    {NULL, 0, 0, 0}
+};
+
+int main(int argc, char *argv[])
 {
-	printf("source: https://github.com/how2flow/deb-template.git\n");
+	if (argc == 1) {
+		printf("source: https://github.com/how2flow/deb-template.git\n");
+		return 0;
+	}
+
+	while ((options =  getopt_long(argc, argv, sopts, lopts, NULL)) != -1) {
+		switch (options) {
+			case 'h':
+				printf("%s: This is normal call\n", argv[0]);
+				break;
+			case '?':
+				/* already printed err message. */
+				break;
+		}
+	}
+
+	return 0;
 }
